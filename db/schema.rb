be1a06b5e8b51b68b5c2c9922ded437f9999ed2b
kohaku428 <daihuku414428@icloud.com>
2021-02-16 21:03:55 +0900

    hairdresserモデル、テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_022413) do
+ActiveRecord::Schema.define(version: 2021_02_16_114935) do
+
+  create_table "hairdressers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_hairdressers_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_02_16_022413) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "hairdressers", "users"
 end
