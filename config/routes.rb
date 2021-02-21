ce1d787bbf17fbdb621420890eb2a9682dfffef1
kohaku428 <daihuku414428@icloud.com>
2021-02-21 17:31:48 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "hairdressers#index"
-  resources :hairdressers
+  resources :hairdressers do
+    resources :appointments, only: [:index, :create]
+  end
 end
